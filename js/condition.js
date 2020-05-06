880157c07e884b064930bbe8ac38954d203d8320
sajeel <sajeel.ahmed786@gmail.com>
2020-05-07 00:57:39 +0500

    Updated If else-if conditions

--- js/condition.js
+++ js/condition.js
@@ -1,4 +1,7 @@
 var age = 25;
+var score = 70;
+// var score = prompt("What's your Score");
+// score = parseInt(score);
 
 if(age >20) {
     console.log("If Condition " + age);
@@ -6,3 +9,19 @@ if(age >20) {
 else {
     console.log("Else Condition " + age);
 }
+
+// with else if condition
+
+if(score > 80){
+    console.log("Grade A");
+}
+else if (score >= 70) {
+    console.log("Grade B");
+}
+else if (score > 60) {
+    console.log("Grade C");
+}
+else {
+    console.log("Failed");
+}
+
