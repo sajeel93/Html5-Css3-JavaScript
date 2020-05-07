50c08e2099a970a6a5f6f35ff79f7a3162fef066
sajeel <sajeel.ahmed786@gmail.com>
2020-05-08 01:12:47 +0500

    Conditions with Logical Operator

--- js/condition.js
+++ js/condition.js
@@ -2,6 +2,7 @@ var age = 25;
 var score = 70;
 // var score = prompt("What's your Score");
 // score = parseInt(score);
+var logicalValve = 5;
 
 if(age >20) {
     console.log("If Condition " + age);
@@ -23,5 +24,22 @@ else if (score > 60) {
 }
 else {
     console.log("Failed");
+}
+
+// Comparison Operators (==, ===(also check data type))
+// Logical Operators (&&(And), ||(OR), !(Not))
+
+// Conditions with Logical Operator
+if (logicalValve > 4 && logicalValve < 6) {
+    console.log(logicalValve);
+    console.log(true);
+} else {
+    console.log("Error");
+}
+
+if (logicalValve > 3 || logicalValve <= 4) {
+    console.log("Using ||(OR) Operator");
+} else {
+    console.log(Error);
 }
 
